94b41e90b65bd27ac6eb614fafc758ebb2a7827b
SammLopes <sam.v.p.lopes@gmail.com>
2024-12-02 00:12:32 -0300

    Update main controller

--- servicemain/src/main/java/com/servicemain/servicemain/controllers/MainController.java
+++ servicemain/src/main/java/com/servicemain/servicemain/controllers/MainController.java
@@ -9,11 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,44 +21,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainController {
 
-    private final S3Service s3Service;
-    private final SqsService sqsService;
     private final ListTask listTask;
 
     @GetMapping
     public String index(Model model) {
 
         var result = listTask.getTasks();
-
-        //Buscar arquivo do S3 
-
         log.info("Teste tela");
         model.addAttribute("tasks", result);
-        model.addAttribute("taskRequest", new TaskRequest());
         return "index";
-    }
 
-    @PostMapping("/save")
-    public String saveTask(@ModelAttribute TaskRequest taskRequest, BindingResult bindingResult) {
-        sqsService.postQueue(taskRequest);
-        //Salvar arquivo do S3 
-        return "redirect:/index";
     }
-
-    @GetMapping("/edit/{id}")
-    public String editTask(@ModelAttribute TaskRequest taskRequest, BindingResult bindingResult) {
-        sqsService.postQueue(taskRequest);
-        //Atualizar arquivo do S3
-        return "index"; 
-    }
-
-    // Método para excluir uma tarefa
-    @PostMapping("/delete/{id}")
-    public String deleteTask(@ModelAttribute TaskRequest taskRequest) {
-        sqsService.postQueue(taskRequest);
-        //dletar arquivo do S3 
-        return "redirect:/index";
-    }
-
 
 }
