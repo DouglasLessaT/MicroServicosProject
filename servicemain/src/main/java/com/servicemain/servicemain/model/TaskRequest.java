18db1a1b3f6471e436e37fadfd9f44f6ca2a74af
SammLopes <sam.v.p.lopes@gmail.com>
2024-12-02 00:12:42 -0300

    Update model task request

--- servicemain/src/main/java/com/servicemain/servicemain/model/TaskRequest.java
+++ servicemain/src/main/java/com/servicemain/servicemain/model/TaskRequest.java
@@ -1,16 +1,24 @@
 package com.servicemain.servicemain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Data
 public class TaskRequest{
 
-    private Long id;
+    private String id;
     private String description;
-    private String urlBucket;
+    private String urlBucketFile;
     private String typeAction ;
+    @JsonIgnore
     private MultipartFile file;
+
+    public TaskRequest() {
+        this.id = UUID.randomUUID().toString();}
+
 }
