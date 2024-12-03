8c93fa43e4296999c28029ba012c08b597b06307
SammLopes <sam.v.p.lopes@gmail.com>
2024-12-03 01:18:36 -0300

    Added model ModelTaskConsumed.java, e SqsService.java

---
+++ servicemain/src/main/java/com/servicemain/servicemain/model/ModelTaskConsumed.java
@@ -0,0 +1,10 @@
+package com.servicemain.servicemain.model;
+
+import lombok.Data;
+
+@Data
+public class ModelTaskConsumed {
+    private String id;
+    private String description;
+    private String urlBucketAws;
+}
--- servicemain/src/main/java/com/servicemain/servicemain/services/SqsService.java
+++ servicemain/src/main/java/com/servicemain/servicemain/services/SqsService.java
@@ -2,6 +2,7 @@ package com.servicemain.servicemain.services;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.servicemain.servicemain.model.ListTask;
+import com.servicemain.servicemain.model.ModelTaskConsumed;
 import com.servicemain.servicemain.model.TaskRequest;
 
 //import io.awspring.cloud.sqs.annotation.SqsListener;
@@ -12,6 +13,7 @@ import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,12 +40,25 @@ public class SqsService {
     @Value("${sqsQueueNamePost}")
     private String filaResposta;
 
-    @SqsListener("${sqsQueueNameConsume}")
+    @SqsListener("toListNotification")
     public void consumeQueue(String message)  {
 
-        log.info("Consume queue {}", message);
+        log.info("Consume queue " + message.toString());
         Object result = convertJsonObject(message);
         listTask.add(result);
+    }
+
+    public synchronized void consumeManually(AmazonSQSAsync amazonSQSAsync) {
+        String queueUrl = amazonSQSAsync.getQueueUrl("toListNotification").getQueueUrl();
+        amazonSQSAsync.receiveMessage(queueUrl).getMessages().forEach(message -> {
+            //TaskRequest result = convertJsonTask(message.getBody());
+            Object result = convertJsonObject(message.getBody());
+
+            log.info("Consume manually " + result.toString());
+            listTask.add(result);
+            log.info("Mensagem consumida manualmente: {}", message.getBody());
+            amazonSQSAsync.deleteMessage(queueUrl, message.getReceiptHandle());
+        });
     }
 
     public synchronized void postQueue(TaskRequest taskRequest){
@@ -59,10 +74,26 @@ public class SqsService {
 
     }
 
-    private Object convertJsonObject(String pagamentoJson){
+    private Object convertJsonObject(String taskJson){
         try {
-            Object task = objectMapper.readValue(pagamentoJson, Object.class);
+            Object task = objectMapper.readValue(taskJson, Object.class);
             return task;
+        } catch (Exception e) {
+            log.error("Erro ao converter JSON para Pagamento: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private TaskRequest convertJsonTask(String taskJson){
+        try {
+            ModelTaskConsumed task = objectMapper.readValue(taskJson, ModelTaskConsumed.class);
+            TaskRequest taskRequest = new TaskRequest();
+
+            taskRequest.setUrlBucketFile(task.getUrlBucketAws());
+            taskRequest.setDescription(task.getDescription());
+            taskRequest.setId(task.getId());
+
+            return taskRequest;
         } catch (Exception e) {
             log.error("Erro ao converter JSON para Pagamento: " + e.getMessage());
             return null;
